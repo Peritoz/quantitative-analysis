a29a5f10065214c43077650d3cac1d5271669469
Diorbert Pereira <diorbert@gmail.com>
2022-03-12 09:50:52 -0300

    Normalizing metrics result

--- src/libs/engines/quantitative_analysis_engine.ts
+++ src/libs/engines/quantitative_analysis_engine.ts
@@ -3,6 +3,7 @@ import PerformanceEngine from "@libs/engines/performance_engine";
 import Model from "@libs/model/model";
 import QuantitativeMetric from "@libs/model/interfaces/quantitative_metric";
 import {formatNumber} from "@libs/utils/format_number";
+import {findRanges, normalizeValue} from "@libs/utils/normalization";
 
 export default class QuantitativeAnalysisEngine {
     workloadEngine: WorkloadEngine;
@@ -13,7 +14,7 @@ export default class QuantitativeAnalysisEngine {
         this.performanceEngine = new PerformanceEngine(model);
     }
 
-    getAllMetrics() {
+    getAllMetrics(includeNormalizedValues: boolean = true) {
         const workloadMetrics = this.workloadEngine.getAllWorkloadsMetrics();
         const metrics: Array<Partial<QuantitativeMetric>> = this.performanceEngine.getAllPerformanceMetrics();
 
@@ -29,10 +30,46 @@ export default class QuantitativeAnalysisEngine {
             }
         }
 
+        // Calculating normalized values
+        if (includeNormalizedValues) {
+            const minMaxMap = findRanges<Partial<QuantitativeMetric>>(metrics);
+
+            for (let i = 0; i < metrics.length; i++) {
+                const metric = metrics[i];
+
+                // Getting normalized workload
+                if (metric.workload !== undefined && minMaxMap["workload"] !== undefined) {
+                    metric.normalizedWorkload = normalizeValue(
+                        metric.workload,
+                        minMaxMap["workload"].min,
+                        minMaxMap["workload"].max
+                    );
+                }
+
+                // Getting normalized response time
+                if (metric.responseTime !== undefined && minMaxMap["responseTime"] !== undefined) {
+                    metric.normalizedResponseTime = normalizeValue(
+                        metric.responseTime,
+                        minMaxMap["responseTime"].min,
+                        minMaxMap["responseTime"].max
+                    );
+                }
+
+                // Getting normalized processing time
+                if (metric.processingTime !== undefined && minMaxMap["processingTime"] !== undefined) {
+                    metric.normalizedProcessingTime = normalizeValue(
+                        metric.processingTime,
+                        minMaxMap["processingTime"].min,
+                        minMaxMap["processingTime"].max
+                    );
+                }
+            }
+        }
+
         return metrics;
     }
 
-    getAllMetricsAsCsv(separator: string) {
+    getAllMetricsAsCsv(separator: string = ";") {
         const sep = separator;
         const metrics = this.getAllMetrics();
         let response = [`resource${sep}service${sep}wLoad${sep}procTime${sep}respTime${sep}util`];
