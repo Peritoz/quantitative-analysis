8d4e7ec4b843232be693991d35568b1e90960f83
Diorbert Pereira <diorbert@gmail.com>
2022-03-12 10:41:22 -0300

    Removing unused function

--- src/libs/utils/normalization.ts
+++ src/libs/utils/normalization.ts
@@ -23,39 +23,3 @@ export function normalizeDistribution<T extends { [key: string]: any }>(
         setter(element, normalizedValue);
     }
 }
-
-/**
- * Finds the range for fields of type number of objects
- * @param metrics Array of objects containing fields of mixed types
- * @return Object mapping containing the minimum and maximum values for each key of type number
- */
-export function findRanges<T extends { [key: string]: any }>(metrics: Array<T>): { [key: string]: { min: number, max: number } } {
-    const rangeMap: { [key: string]: { min: number, max: number } } = {};
-
-    // Getting the minimum and maximum
-    for (let i = 0; i < metrics.length; i++) {
-        const metric = metrics[i];
-
-        // For each key of type number, set minimum and maximum
-        for (const key in metric) {
-            if (metric.hasOwnProperty(key) && typeof metric[key] === "number") {
-                const value: number = metric[key];
-                const mappedRange = rangeMap[key];
-
-                if (mappedRange === undefined) {
-                    rangeMap[key] = {min: value, max: value};
-                } else {
-                    if (value < mappedRange.min) {
-                        rangeMap[key].min = value;
-                    }
-
-                    if (value > mappedRange.max) {
-                        rangeMap[key].max = value;
-                    }
-                }
-            }
-        }
-    }
-
-    return rangeMap;
-}
