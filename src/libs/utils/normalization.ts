ce9063047043fc51013f7a95b735298dac229218
Diorbert Pereira <diorbert@gmail.com>
2022-03-12 09:08:02 -0300

    Creating function to find ranges of object "number" fields

--- src/libs/utils/normalization.ts
+++ src/libs/utils/normalization.ts
@@ -1,17 +1,34 @@
-export function normalizeDistribution(distribution: Array<number>): Array<number> {
-    const normalizedArray: Array<number> = [];
-    const min = Math.min(...distribution);
-    const max = Math.max(...distribution);
-
-    for (let i = 0; i < distribution.length; i++) {
-        const value = distribution[i];
-
-        normalizedArray.push(normalizeValue(value, min, max));
-    }
-
-    return normalizedArray;
-}
-
 export function normalizeValue(value: number, min: number, max: number): number {
     return (value - min) / max;
+}
+
+export function findRanges<T extends { [key: string]: any }>(metrics: Array<T>): { [key: string]: { min: number, max: number } } {
+    const rangeMap: { [key: string]: { min: number, max: number } } = {};
+
+    // Getting the minimum and maximum
+    for (let i = 0; i < metrics.length; i++) {
+        const metric = metrics[i];
+
+        // For each key of type number, set minimum and maximum
+        for (const key in metric) {
+            if (metric.hasOwnProperty(key) && typeof metric[key] === "number") {
+                const value: number = metric[key];
+                const mappedRange = rangeMap[key];
+
+                if (mappedRange === undefined) {
+                    rangeMap[key] = {min: value, max: value};
+                }else{
+                    if(value < mappedRange.min){
+                        rangeMap[key].min = value;
+                    }
+
+                    if(value > mappedRange.max){
+                        rangeMap[key].max = value;
+                    }
+                }
+            }
+        }
+    }
+
+    return rangeMap;
 }
